ea3c178f0f2c451d23342663365bb7f8371e6885
Chloe Macias <chloeamacias@gmail.com>
2023-02-15 14:29:43 -0500

    NavBar and UserLogin components created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Homepage from "./components/Homepage";
+import UserLogin from "./components/UserLogin";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/login" element = {<UserLogin/>}>
+                </Route>
+                <Route path="/" element = {<Homepage/>}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
